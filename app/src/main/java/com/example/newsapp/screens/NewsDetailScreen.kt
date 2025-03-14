dc991628899a43d660461d15e472c439bad32c4a
harshrawat22202 <harsh22202@iiitd.ac.in>
2025-03-14 20:39:40 +0530

    Article Screen

--- app/src/main/java/com/example/newsapp/screens/NewsDetailScreen.kt
+++ app/src/main/java/com/example/newsapp/screens/NewsDetailScreen.kt
@@ -1,5 +1,8 @@
 package com.example.newsapp.screens
 
+import android.annotation.SuppressLint
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -9,47 +12,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.example.newsapp.api.Article
 
+@SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun NewsDetailScreen(navController: NavController, article: Article) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
-    ) {
-        AsyncImage(
-            model = article.urlToImage,
-            contentDescription = article.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = article.title,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Text(
-            text = "By ${article.author} | Published: ${article.publishedAt}",
-            fontSize = 14.sp,
-            color = MaterialTheme.colorScheme.secondary
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = article.description,
-            fontSize = 16.sp
-        )
-    }
+    val url = article.url
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                webViewClient= WebViewClient()
+                loadUrl(url)
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
 }
