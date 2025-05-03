27e05d8f0339b51234e73c08a13ed1b2f6b85398
harshrawat22202 <harsh22202@iiitd.ac.in>
2025-05-03 18:34:57 +0530

    Battery Implemented

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val themeViewModel = ThemeViewModel()
+        val themeViewModel = ThemeViewModel(application)
         setContent {
             val theme by themeViewModel.theme.collectAsState()
 
--- app/src/main/java/com/example/newsapp/screens/ThemeViewModel.kt
+++ app/src/main/java/com/example/newsapp/screens/ThemeViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.newsapp.screens
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 
-class ThemeViewModel:ViewModel() {
+class ThemeViewModel(application: Application):AndroidViewModel(application) {
     private val _theme= MutableStateFlow<String>("System")
     val theme= _theme
+    private val context=application.applicationContext
+
+    init{
+        checkBatteryLevel()
+    }
 
     fun setTheme(theme: String) {
         _theme.value = theme
@@ -16,6 +25,22 @@ class ThemeViewModel:ViewModel() {
     }
 
     fun toggleTheme() {
-        _theme.value = if (_theme.value == "System") "Light" else "System"
+        if (_theme.value == "System" || _theme.value == "Dark")
+            _theme.value="Light"
+        else
+            _theme.value="Dark"
+    }
+
+    fun checkBatteryLevel() {
+        val batteryIntent = context.applicationContext.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        val level = batteryIntent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale = batteryIntent?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+
+        if (level >= 0 && scale > 0) {
+            val batteryPct = (level * 100) / scale
+            if (batteryPct <= 15) {
+                _theme.value = "Dark"
+            }
+        }
     }
 }
